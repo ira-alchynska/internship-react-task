0c590fa522d77b2208ee46a5f89d4bd8d1b35967
Iryna Alchynska <iren0988@gmail.com>
2021-12-08 22:16:09 +0200

    modal window and form

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,36 +24,28 @@ export const store = createStore(
   composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
-store.subscribe((type) => {
-  store.getState();
+store.subscribe(() => {
+  let tempStore = store.getState();
+  switch (true) {
+    case SET_HIDE_COLUMNS === "HIDE_COLUMNS":
+      console.log("switch1");
+      localStorage.setItem(
+        "hiddenColumns",
+        JSON.stringify(tempStore.country.hiddenColumns)
+      );
 
-  switch (type) {
-    case SET_HIDE_COLUMNS: {
-      localStorage.setItem("hiddenColumns", JSON.stringify([]));
-    }
-    case SET_FILTER_VALUE: {
-      localStorage.setItem("filterValue", JSON.stringify(""));
-    }
-    case SET_SORT_COUNTRIES: {
-      localStorage.setItem("sortColumnOrder", JSON.stringify({}));
-    }
+    case SET_FILTER_VALUE === "SET_FILTER_VALUE":
+      console.log("switch2");
+      localStorage.setItem(
+        "filterValue",
+        JSON.stringify(tempStore.country.filterValue)
+      );
+
+    case SET_SORT_COUNTRIES === "SORT_COUNTRIES":
+      console.log("switch3");
+      localStorage.setItem(
+        "sortColumnOrder",
+        JSON.stringify(tempStore.country.sortColumnOrder)
+      );
   }
 });
-// function select(state) {
-//   return state.some.deep.property
-// }
-
-// let currentValue
-// function handleChange() {
-//   let previousValue = currentValue
-//   currentValue = select(store.getState())
-
-//   if (previousValue !== currentValue) {
-//     console.log(
-//       'Some deep nested property changed from',
-//       previousValue,
-//       'to',
-//       currentValue
-//     )
-//   }
-// }
