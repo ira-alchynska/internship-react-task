89c79741213a8f5ea3bd26f6345486f73e3e0ab8
Iryna Alchynska <iren0988@gmail.com>
2021-12-22 09:37:10 +0200

    cards

--- src/components/CountryCard/CountryCardList.jsx
+++ src/components/CountryCard/CountryCardList.jsx
@@ -1,32 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CountriesSelectors from "../../redux/countries/selectors.js";
-//import CountryCardItem from "./CountryCardItem";
+import { setFilterValue } from "../../redux/countries/actions";
 import { fetchCountries } from "../../redux/countries/actions.js";
 import { useSelector, useDispatch } from "react-redux";
+import "./styles.css";
 const CountryCardList = () => {
   const dispatch = useDispatch();
 
-  const countriesData = useSelector(CountriesSelectors.selectCountriesData);
+  const [page, setPage] = useState(1);
 
-  // console.log("data", countriesData);
+  const countriesData = useSelector(CountriesSelectors.selectFilteredCountries);
+  const filterValue = useSelector(CountriesSelectors.selectFilterValue);
 
   useEffect(() => {
-    dispatch(fetchCountries());
-  }, []);
+    if (!countriesData.length || page !== 1) {
+      dispatch(fetchCountries(page));
+    }
+
+    return () => {
+      dispatch(setFilterValue(""));
+    };
+  }, [page]);
+
+  const handleChange = (e) => {
+    dispatch(setFilterValue(e.target.value));
+  };
 
   return (
     <>
-      <ul className="flag-list">
-        {countriesData.map((country) => {
-          const { id, flag, name } = country;
+      <div className="card-wrapper">
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          value={filterValue}
+          className="flag-input"
+          placeholder="Enter country name"
+          aria-label="Recipient's username"
+          onChange={handleChange}
+        />
+        <ul className="flag-list">
+          {countriesData.map((country) => {
+            const { id, flag, name, capital } = country;
 
-          return (
-            <li key={id} className="flag-item">
-              <img src={flag} alt={name} />
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li key={id} className="flag-item">
+                <div className="container">
+                  <h2 className="name">{name}</h2>
+                  <img
+                    src={flag ?? "/flags/default.png"}
+                    alt="flag"
+                    className="flag"
+                  />
+                  <p className="capital">Capital: {capital}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          className="btn-primary"
+          type="button"
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          Load more...
+        </button>
+      </div>
     </>
   );
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,9 @@ import Login from "../components/LogIn/Login";
 
 const LoginPage = () => {
   return (
-    <div>
+    <>
       <Login />
-    </div>
+    </>
   );
 };
 
